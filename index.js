2ed15ca81a2bf876607a154be51b4f8cd67dc192
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-01-04 06:46:36 +0600

    categories route set

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv'
+import cors from 'cors';
 dotenv.config()
 const app = express();
 const port = process.env.PORT || 3000;
 
-console.log(process.env.DB_USERNAME)
+// middleware
+app.use(cors())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.clipjzr.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,12 +25,53 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+
+        // mongodb collection
+        const menuCollection = client.db("bistroDB").collection("menu");
+
+        app.get("/menu", async (req, res) => {
+            try {
+                const result = await menuCollection.find().toArray();
+                res.send(result);
+            } catch (err) {
+                res.status(500).json({ message: 'Error fetching unique categories', error: err.message });
+            }
+        })
+
+        app.get("/categories", async (req, res) => {
+            try {
+                const options = {
+                    // Include only the `category` fields in each returned document
+                    projection: { _id: 0, category: 1 },
+                };
+
+                const allItems = await menuCollection.find({}, options).toArray();
+                const uniqueCategories = [...new Set(allItems.map(item => item.category))];
+                res.send(uniqueCategories);
+            } catch (error) {
+                console.error("Error fetching distinct categories:", error);
+                res.status(500).send("Internal Server Error");
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
@@ -35,7 +79,7 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("server in running")
+    res.send("Server in running!")
 });
 
 app.listen(port, () => {
